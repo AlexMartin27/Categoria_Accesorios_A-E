85edc3ed2e1050c0828de29e1b19b1bade1fd81c
Alex Martinez <martinez49521@gmail.com>
2024-06-26 18:20:41 -0600

    primer

---
+++ app.js
@@ -0,0 +1,748 @@
+let products = {
+    data:[{
+        productName: "Mariposa Celeste",
+        category: "Pulceras",
+        price: "3.00",
+        image: "img/pulceras/mariposa_celeste.jpeg",
+  },
+  {
+    productName: "Mariposa Rosa",
+    category: "Pulceras",
+    price: "3.00",
+    image: "img/pulceras/mariposa_rosa.jpeg",
+},
+{
+    productName: "Pulcera de Cruz",
+    category: "Pulceras",
+    price: "3.00",
+    image: "img/pulceras/pulcera_cruz.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE ROSA",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_rosas.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE CORAZONES",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_corazones.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE MARIPOSA",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_mariposas.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE MARIPOSAS",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_maripo.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE LIBELULA",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_libelula.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE MANZANA",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_manzana.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE MANZANAS",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_manzanaHueca.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE FLOR",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_flor.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE TRIANGULO",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_triangulo.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE LIBELULAS",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadoras_libelulas2.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE ESTRELLAS",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_estrella.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE CORAZON",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/violadores/violadores_corazon.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE CUADRADO",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/Violadores/violadores_cuadrado.jpg",
+},
+{
+    productName: "VIOLADORES CON FORMA DE ROMBO",
+    category: "Violadores",
+    price: "1.00",
+    image: "img/Violadores/violadores_rombo.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE LIBELULA",
+    category: "Aretes",
+    price: "1.50",
+    image: "img/aretes/aretes_largos_libelula.jpg",
+},
+{
+    productName: "ARETE PEQUEÑO",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/aritos_pequeños_uno.jpg",
+},
+{
+    productName: "ARETES PEQUEÑOS",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/aritos_pequeños_dos.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE CRUZ",
+    category: "Aretes",
+    price: "1.50",
+    image: "img/aretes/aretes_largos.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE CIRCULO Y ESTRELLA",
+    category: "Aretes",
+    price: "1.50",
+    image: "img/aretes/aritos_grandes_circulo.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE ESTRELLA",
+    category: "Aretes",
+    price: "1.50",
+    image: "img/aretes/aritos_largos_estrella.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE PELOTITA",
+    category: "Aretes",
+    price: "2.50",
+    image: "img/aretes/aretes_largos_bolitas.jpg",
+},
+{
+    productName: "ARETES LARGOS CON FORMA DE TRIANGULO",
+    category: "Aretes",
+    price: "2.50",
+    image: "img/aretes/aretes_largos_triangulo.jpg",
+},
+{
+    productName: "ARETES LARGOS",
+    category: "Aretes",
+    price: "1.50",
+    image: "img/aretes/aretes largos.jpg",
+},
+{
+    productName: "ARITOS PEQUEÑOS",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_pequeño_uno.jpg",
+},
+{
+    productName: "ARITO PEQUEÑO",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_pequeños_dos.jpg",
+},
+{
+    productName: "ARITO MARIPOSA AZUL",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_mariposa azul.jpg",
+},
+{
+    productName: "ARITO MARIPOSA NEGRO",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_mariposa_negro.jpg",
+},
+{
+    productName: "ARITO MARIPOSA ROJA",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_mariposa_rojo.jpg",
+},
+{
+    productName: "ARITO MARIPOSA BLANCO",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_mariposa_blanco.jpg",
+},
+{
+    productName: "ARITO MARIPOSA MORADO",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_mariposa_morado.jpg",
+},
+{
+    productName: "ARITO GIRASOL",
+    category: "Aretes",
+    price: "0.75",
+    image: "img/aretes/arito_girasol.jpg",
+},
+{
+    productName: "ARGOLLAS PEQUEÑITAS",
+    category: "Argollas",
+    price: "1.50",
+    image: "img/argollas/argolla_pequeñita.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO TREVOL",
+    category: "Argollas",
+    price: "1.50",
+    image: "img/argollas/argollas_acero_trevol.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO LIBELULA",
+    category: "Argollas",
+    price: "1.50",
+    image: "img/argollas/argolla_acero_libelula.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO MARIPOSA",
+    category: "Argollas",
+    price: "1.50",
+    image: "img/argollas/argolla_mariposa.jpg",
+},
+{
+    productName: "ARGOLLAS MEDIANAS DE ACERO FLOR",
+    category: "Argollas",
+    price: "1.25",
+    image: "img/argollas/argolla_grande_estrella_amarilla.jpg",
+},
+{
+    productName: "ARGOLLAS MEDIANAS DE ACERO FLOR color plata",
+    category: "Argollas",
+    price: "1.25",
+    image: "img/argollas/argolla_grande_estrella_gris.jpg",
+},
+{
+    productName: "ARGOLLAS PEQUEÑAS DE ACERO FLOR (PLATA)",
+    category: "Argollas",
+    price: "0.75",
+    image: "img/argollas/argolla_gris_pequeña.jpg",
+},
+{
+    productName: "ARGOLLAS PEQUEÑAS DE ACERO FLOR (DORADO)",
+    category: "Argollas",
+    price: "0.75",
+    image: "img/argollas/argolla_amarilla_pequeña.jpg",
+},
+{
+    productName: "ARGOLLAS PEQUEÑAS DE ACERO CORAZON (PLATA)",
+    category: "Argollas",
+    price: "0.75",
+    image: "img/argollas/argolla_gris_pequeña_corazon.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO HOJAS (DORADO)",
+    category: "Argollas",
+    price: "1.00",
+    image: "img/argollas/argolla_amarilla_oja.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO MEDIANAS (DORADO)",
+    category: "Argollas",
+    price: "1.00",
+    image: "img/argollas/argolla_mediana_amarillo.jpg",
+},
+{
+    productName: "ARGOLLAS DE ACERO GRANDE (PLATA)",
+    category: "Argollas",
+    price: "1.25",
+    image: "img/argollas/argollas_grande.jpg",
+},
+{
+    productName: "COLLAR DE ACERO EN FORMA DE CORAZON",
+    category: "Collares",
+    price: "2.00",
+    image: "img/collares/collar_acero_corazon.jpg",
+},
+{
+    productName: "COLLAR DE ACERO EN FORMA DE MUELA",
+    category: "Collares",
+    price: "2.00",
+    image: "img/collares/collar_muela.jpg",
+},
+{
+    productName: "COLLAR DE ACERO EN FORMA DE LIBELULA",
+    category: "Collares",
+    price: "2.00",
+    image: "img/collares/collar_libelula.jpg",
+},
+{
+    productName: "COLLAR DE ACERO MARGARITA",
+    category: "Collares",
+    price: "2.00",
+    image: "img/collares/collar_Acero.jpg",
+},
+{
+    productName: "COLLAR DE ACERO TREVOL",
+    category: "Collares",
+    price: "4.00",
+    image: "img/collares/collar_acero_trevol.jpg",
+},
+{
+    productName: "COLLAR DE ACERO MARIPOSA",
+    category: "Collares",
+    price: "4.00",
+    image: "img/collares/collar_Acero_mariposa.jpg",
+},
+{
+    productName: "COLLAR DE ACERO GRANDE",
+    category: "Collares",
+    price: "4.00",
+    image: "img/collares/collar_perlitas.jpg",
+},
+{
+    productName: "ANILLO DE CORAZON TALLA:20",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_corazon.jpg",
+},
+{
+    productName: "ANILLO DE CRUZ TALLA:18",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_cruz.jpg",
+},
+{
+    productName: "ANILLO DE MARIPOSAS TALLA:20",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_mariposa.jpg",
+},
+{
+    productName: "ANILLO DE MARIPOSA TALLA:20",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_mariposa_uno.jpg",
+},
+{
+    productName: "ANILLO DE TIJERAS TALLA:20",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_tirjera.jpg",
+},
+{
+    productName: "ANILLO DE CORONAS TALLA:16",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_coronas.jpg",
+},
+{
+    productName: "ANILLO DE ESTRELLA TALLA:19",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_estrellita.jpg",
+},
+{
+    productName: "ANILLO DE ESTRELLA DE MAR TALLA:17",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_estrella_mar.jpg",
+},
+{
+    productName: "ANILLO DE NOTA MUSICAL AJUSTABLE",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_nota_musical.jpg",
+},
+{
+    productName: "ANILLO DE ACERO",
+    category: "Anillos",
+    price: "2.50",
+    image: "img/anillos/anillo_acero.jpg",
+},
+{
+    productName: "TOBILLERA DE ESTRELLAS",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_estrella.jpg",
+},
+{
+    productName: "TOBILLERA DE TRIANGULO",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_triangulo.jpg",
+},
+{
+    productName: "TOBILLERA DE MARIPOSAS",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_mariposa.jpg",
+},
+{
+    productName: "TOBILLERA DE ELEFANTE",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_elefante.jpg",
+},
+{
+    productName: "TOBILLERA DE GATITO",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_gatita.jpg",
+},
+{
+    productName: "HERMOSA TOBILLERA",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/Tobillera_hermosa.jpg",
+},
+{
+    productName: "TOBILLERA DE RAFAGA",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_linda.jpg",
+},
+{
+    productName: "TOBILLERA DE LLAVES",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_llaves.jpg",
+},
+{
+    productName: "TOBILLERA DE ROMBOS",
+    category: "Tobilleras",
+    price: "3.00",
+    image: "img/tobilleras/tobillera_rombito.jpg",
+},
+{
+    productName: "GANCHO DE FLOR AZUL",
+    category: "Ganchos",
+    price: "1.00",
+    image: "img/ganchos/gancho_flor_azul.jpg",
+},
+{
+    productName: "GANCHO DE FLOR GRIS",
+    category: "Ganchos",
+    price: "1.00",
+    image: "img/ganchos/gancho_flor_gris.jpg",
+},
+{
+    productName: "GANCHO GRIS",
+    category: "Ganchos",
+    price: "1.25",
+    image: "img/ganchos/gancho_gris.jpg",
+},
+{
+    productName: "GANCHO DORADO",
+    category: "Ganchos",
+    price: "1.25",
+    image: "img/ganchos/gancho_dorado.jpg",
+},
+{
+    productName: "GANCHOS GRIS",
+    category: "Ganchos",
+    price: "1.25",
+    image: "img/ganchos/ganchos_gris.jpg",
+},
+{
+    productName: "GANCHOS DORADO",
+    category: "Ganchos",
+    price: "1.25",
+    image: "img/ganchos/ganchos_dorado.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS AMARILLAS",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_amarilla.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS GRIS",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_gris.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS ROSADO OSCURO",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_rosada_oscura.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS ROSADO CLARO",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_rosada_clara.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS CAFE",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_cafe.jpg",
+},
+{
+    productName: "LLAVERO DE BOLITAS PIEL",
+    category: "Llaveros",
+    price: "1.00",
+    image: "img/llaveros/bolita_piel.jpg",
+},
+{
+    productName: "LLAVERO DE CONEJO AZUL",
+    category: "Llaveros",
+    price: "2.50",
+    image: "img/llaveros/conejito_azul.jpg",
+},
+{
+    productName: "LLAVERO DE CONEJO CAFE",
+    category: "Llaveros",
+    price: "2.50",
+    image: "img/llaveros/conejito_cafe.jpg",
+},
+{
+    productName: "LLAVERO DE CONEJO GRIS",
+    category: "Llaveros",
+    price: "2.50",
+    image: "img/llaveros/conejito_gris.jpg",
+},
+{
+    productName: "LLAVERO DE CONEJO ROSADO",
+    category: "Llaveros",
+    price: "2.50",
+    image: "img/llaveros/conejito_rosado.jpg",
+},
+{
+    productName: "CABLE TIPO V8 CARGA RAPIDA 6A",
+    category: "Cables",
+    price: "2.00",
+    image: "img/cables/cable_usb.jpg",
+},
+{
+    productName: "VIDRIO TEMPLADO 7PLUS",
+    category: "Vidrios",
+    price: "1.00",
+    image: "img/vidrios/vidrio_templado_7plus.jpg",
+},
+{
+    productName: "VIDRIO TEMPLADO A21S",
+    category: "Vidrios",
+    price: "1.00",
+    image: "img/vidrios/vidrio_templado_A21s.jpg",
+},
+{
+    productName: "VIDRIO TEMPLADO A12 GT",
+    category: "Vidrios",
+    price: "1.00",
+    image: "img/vidrios/vidrio_templado_A12.jpg",
+},
+{
+    productName: "PROTECTOR PARA SAMSUNG A31",
+    category: "Protector",
+    price: "5.00",
+    image: "img/protector/protector_a31.jpg",
+},
+{
+    productName: "PROTECTOR PARA IPHONE XR",
+    category: "Protector",
+    price: "5.00",
+    image: "img/protector/protector_iphone_xr.jpg",
+},
+{
+    productName: "PROTECTOR PARA IPHONE XR MAX",
+    category: "Protector",
+    price: "5.00",
+    image: "img/protector/Protector_para_Iphone_XR_max.jpg",
+},
+{
+    productName: "PROTECTOR PARA SAMSUNG A12",
+    category: "Protector",
+    price: "5.00",
+    image: "img/protector/protector_a12.jpg",
+},
+{
+    productName: "PROTECTOR PARA POCO X3 GT",
+    category: "Protector",
+    price: "5.00",
+    image: "img/protector/poco_x3_gt.jpg",
+},
+{
+    productName: "BLUSA TALLA S",
+    category: "Blusas",
+    price: "6.00",
+    image: "img/blusas/camisa 7.png",
+},
+{
+    productName: "BLUSA TALLA M",
+    category: "Blusas",
+    price: "6.00",
+    image: "img/blusas/camisa 8 m.png",
+},
+{
+    productName: "BLUSA TALLA M",
+    category: "Blusas",
+    price: "6.00",
+    image: "img/blusas/camisa 9 m.png",
+},
+{
+    productName: "BLUSA TALLA M",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa.png",
+},
+{
+    productName: "BLUSA TALLA M",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa 2.png",
+},
+{
+    productName: "BLUSA TALLA S",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa 3.png",
+},
+{
+    productName: "BLUSA TALLA L",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa 4 L.png",
+},
+{
+    productName: "BLUSA TALLA S",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa 5 s.png",
+},
+{
+    productName: "BLUSA TALLA S",
+    category: "Blusas",
+    price: "4.00",
+    image: "img/blusas/camisa 6 s.png",
+},
+],
+};
+
+for( let i of products.data){
+    //Create card
+    let card = document.createElement("div");
+    //Card should have category and should stay hidden initially
+    card.classList.add("card",i.category, "hide");
+    // img div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //img tag
+    let image = document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let name = document.createElement("h5");
+    name.classList.add("product-name");
+    name.innerText = i.productName.toUpperCase();
+    container.appendChild(name);
+    
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "$" + i.price;
+    container.appendChild(price);
+    
+    
+    card.appendChild(container);
+    document.getElementById("products").appendChild(card);
+    }
+    
+    //parameter passed from button (Parameter same as  category)
+    
+    function filterProduct(value){
+        //Buttom class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach(button =>{
+        // check if equals innerText
+        if(value.toUpperCase() == button.innerText.toUpperCase()){
+            button.classList.add("active");
+        }else{
+            button.classList.remove("active");
+        }  
+    });
+    
+    //select all cards
+    let elements = document.querySelectorAll(".card");
+    //loop through all cards
+    elements.forEach((element) =>{
+        //display all cards on 'all' button click
+        if(value == "all"){
+            element.classList.remove("hide");
+        }
+        else{
+            //Check if element contains category class
+            if(element.classList.contains(value)){
+                //display element based on category
+                element.classList.remove("hide");
+            }else{
+                // hide other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+    }
+    
+    //Search button click
+    document.getElementById("search").addEventListener("click", () => {
+        //initialization 
+        let searchInput = document.getElementById("search-input").value;
+        let elements = document.querySelectorAll(".product-name");
+        let cards = document.querySelectorAll(".card");
+    
+        //loop through all elements
+        elements.forEach((element,index) =>{
+            //check if text includes the search value
+            if(element.innerText.includes(searchInput.toUpperCase())){
+                //display matching card
+                cards[index].classList.remove("hide");
+            }else{
+                //hide others
+                cards[index].classList.add("hide");
+            }
+        })
+    
+    });
+    
+    //Initially display all products
+    window.onload = () =>{
+        filterProduct("all");
+    };
+
+
+    // para ir arriba de la pagina
+    function irArriba(){
+        window.addEventListener('scroll', () =>{
+            var scroll = document.documentElement.scrollTop;
+            var botonArriba = document.getElementById('botonArriba');
+            if(scroll > 300){
+                botonArriba.style.right =  20 + "px";
+            }else{
+                botonArriba.style.right =  -100 + "px";
+            }
+        })
+    }
+
+    irArriba();
